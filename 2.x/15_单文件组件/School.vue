4e95a23326bbf831429db746f0005325cea5a22b
xxxerxes <xx_xerxes@163.com>
2022-05-19 17:35:16 +0800

    更新

---
+++ 2.x/15_单文件组件/School.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="demo">
+    <h3>姓名：{{ name }}</h3>
+    <h3>年龄：{{ age }}</h3>
+    <button @click="show">点我</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "xx",
+      age: 18,
+    };
+  },
+  methods: {
+    show() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+
+<style>
+.demo {
+  background-color: orange;
+}
+</style>
